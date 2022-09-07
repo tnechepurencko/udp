363b2b418c805d062b56506ab8daa9fa7c60f9cf
tatiana <monday240220@gmail.com>
2022-09-07 13:13:29 +0300

    stuff

--- client.py
+++ client.py
@@ -1,17 +1,23 @@
 import socket
+import sys
 
 host = 'localhost'
 port = 8080
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.settimeout(3)
+timeout = 0.5
+s.settimeout(timeout)
 
 # STEP 1: send the hello message (message index = 0), receive the ack (message index = 1)
 
 hello_msg = '0hello from client'.encode('utf-8')
 buffer_size = 0
 
+send_count = 0
 while True:
+    send_count += 1
+    if send_count >= 5:
+        sys.exit(1)
     s.sendto(hello_msg, (host, port))
     try:
         data = s.recv(1024)
@@ -25,6 +31,22 @@ while True:
             break
 
 # STEP 2: send the file (message index = 2), receive the ack (message index = 3)
+send_count = 0
+filename = 'theFile'
+while True:
+    send_count += 1
+    if send_count >= 5:
+        sys.exit(1)
+    s.sendto(f'20 {filename}'.encode(), (host, port))
+    try:
+        data = s.recv(1024)
+    except socket.timeout:
+        print('!time out for file message!')
+    else:
+        data = data.decode('utf-8')
+        if data[0] == '3' and data[1:data.find(' ')] == str(0):
+            print(data[data.find(' ') + 1:])
+            break
 
 file = open('file1.txt', 'rb')
 chunk_count = 0
@@ -33,7 +55,11 @@ while True:
     chunk_count += 1
     if not chunk:
         break
+    send_count = 0
     while True:
+        send_count += 1
+        if send_count >= 5:
+            sys.exit(1)
         s.sendto(f'2{chunk_count} '.encode() + chunk, (host, port))
         try:
             data = s.recv(1024)
@@ -42,12 +68,16 @@ while True:
         else:
             data = data.decode('utf-8')
             if data[0] == '3' and data[1:data.find(' ')] == str(chunk_count):
-                print(data[data.find(' ') + 1:], ' \"', chunk.decode(), '\"', sep='')
+                print(data[data.find(' ') + 1:], chunk_count)
                 break
 
 # STEP 3: send the final message (message index = 4), receive the ack (message index = 5)
 
+send_count = 0
 while True:
+    send_count += 1
+    if send_count >= 5:
+        sys.exit(1)
     s.sendto('4end of file'.encode(), (host, port))
     try:
         data = s.recv(1024)
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+import time
 
 host = 'localhost'
 port = 8080
@@ -10,12 +12,39 @@ s.bind(("", port))
 clients = {}
 chunk_size = 2
 
+
+class Timer(threading.Thread):
+    def __init__(self, client_addr):
+        super().__init__()
+        self.client_address = client_addr
+        self.time_to_close_session = False
+
+    def run(self):
+        super().run()
+        time.sleep(3)
+        self.time_to_close_session = True
+
+    @staticmethod
+    def file_transfer_is_finished():
+        time.sleep(1)
+        clients.pop(client_address)
+
+    def try_to_close_session(self):
+        if self.time_to_close_session:
+            clients.pop(client_address)
+
+
+got_first_message = False
 while True:
     try:
         client_msg, client_address = s.recvfrom(1024)
     except socket.timeout:
         print('!time out: waiting for a new message!')
     else:
+        if got_first_message:
+            timer.try_to_close_session()
+        else:
+            got_first_message = True
         client_msg = client_msg.decode('utf-8')
         if client_msg[0] == '0':
             if client_address not in clients.keys():
@@ -25,17 +54,30 @@ while True:
             reply = f'1{chunk_size} server received the hello message'.encode('utf-8')
             s.sendto(reply, client_address)
         elif client_msg[0] == '2':
+            if len(clients[client_address]) == 0:
+                print(f'the session of client {client_address} is closed')
+                continue
             if len(clients[client_address]) == 1:
                 clients[client_address].append('')
-            chunk = client_msg[client_msg.find(' ') + 1:]
-            if len(clients[client_address][1]) < chunk_size * int(client_msg[1:client_msg.find(' ')]):
-                clients[client_address][1] += chunk
+            if client_msg[1] == '0':
+                clients[client_address].append(open(client_msg[3:], 'wb'))
+                clients[client_address][1] += '0'
+            chunk_index = client_msg.find(' ')
+            if clients[client_address][1][-1] != client_msg[1:chunk_index] or len(clients[client_address][1]) == 0:
+                clients[client_address][1] += client_msg[1:chunk_index]
+                clients[client_address][2].write(client_msg[chunk_index + 1:].encode())
             reply = f'3{client_msg[1]} server received the chunk'.encode('utf-8')
-            print(f'chunk \"{chunk}\" is received')
+            print(f'chunk {client_msg[1:chunk_index]} is received')
             s.sendto(reply, client_address)
         elif client_msg[0] == '4':
+            if len(clients[client_address]) == 0:
+                print(f'the session of client {client_address} is closed')
+                continue
             reply = '5server received the file'.encode('utf-8')
             s.sendto(reply, client_address)
-            print('file:', clients[client_address][1])
             print('data transfer is finished')
-            clients.pop(client_address)
+            timer.file_transfer_is_finished()
+        timer = Timer(client_address)
+        timer.start()
+
+
